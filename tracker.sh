7dd426728d83ab1083fee33da389045b78b38559
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2019-05-12 13:38:35 +0900

    c

--- tracker.sh
+++ tracker.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-ls
-while :
+
+cd
+cd pi_tracker
+python tracker.py
+cd
+files="/home/pi/repo_tracker/storage/*"
+for pathfile in $files
 do
-  cd
-  cd pi_tracker
-  python tracker.py
-  cd
-  files="/home/pi/repo_tracker/storage/*"
-  for pathfile in $files
-  do
-    echo $pathfile
-    name="${pathfile##*/}"
-    echo $name
-    ./dropbox_uploader.sh upload $pathfile /home/pi/projects/pi_tracker/$name
-    rm $pathfile
-  done
+  echo $pathfile
+  name="${pathfile##*/}"
+  echo $name
+  ./dropbox_uploader.sh upload $pathfile /home/pi/projects/pi_tracker/$name
+  rm $pathfile
 done
+
 
